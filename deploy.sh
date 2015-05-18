f32ca3a5a836019bf9623da0ef53fdfd15190316
Marc Jones <marc.jones@jic.ac.uk>
2015-05-18 14:41:40 +0100

    Initial deployment

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project. 
+hugo -t hyde
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+git commit -m "rebuilding site `date`"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
